c51462462ff9a88e638e6dac4124f8d15982f58a
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-02 05:11:00 -0400

    this mod will now be source code based

--- assets/preload/data/Static-Hit/TimeBar.lua
+++
@@ -1,6 +0,0 @@
-function onBeatHit()
-
-     if curBeat %1 == 0 then
-        setProperty('timeBar.color', getColorFromHex('660000'))
-end
-end
--- assets/preload/data/knowledge/TimeBar.lua
+++
@@ -1,6 +0,0 @@
-function onBeatHit()
-
-     if curBeat %1 == 0 then
-        setProperty('timeBar.color', getColorFromHex('ED83AA'))
-end
-end
