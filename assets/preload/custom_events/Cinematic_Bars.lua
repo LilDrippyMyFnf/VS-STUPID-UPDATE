bc2e65691051ca97ed22bdaceb7122d572e1f8e2
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-08 11:54:19 -0400

    junk a hunk shit

--- assets/preload/custom_events/Cinematic_Bars.lua
+++ assets/preload/custom_events/Cinematic_Bars.lua
@@ -1,46 +1,94 @@
--- Event Working
-startValue = 0
+--Created by RamenDominoes (Please credit if using this, thanks! <3)
 
-function onCreate()
-	-- Bar Sprites
-	makeLuaSprite('topBar', 'empty', 0, -120)
-	makeGraphic('topBar', 1280, 120, '000000')
-	setObjectCamera('topBar', 'hud')
-	addLuaSprite('topBar', false)
+HudAssets = {'healthBarBG','healthBar','scoreTxt','iconP1','iconP2','timeBar','timeBarBG','timeTxt'}
+Index = 1
 
-	makeLuaSprite('bottomBar', 'empty', 0, 720)
-	makeGraphic('bottomBar', 1280, 120, '000000')
-	setObjectCamera('bottomBar', 'hud')
-	addLuaSprite('bottomBar', false)
+function onCreatePost()
+
+    makeLuaSprite('UpperBar(UnderLap)', 'empty', -110, -350)
+	makeGraphic('UpperBar(UnderLap)', 1500, 350, '000000')
+	setObjectCamera('UpperBar(UnderLap)', 'HUD')
+	addLuaSprite('UpperBar(UnderLap)', false)
+
+    makeLuaSprite('LowerBar(UnderLap)', 'empty', -110, 720)
+	makeGraphic('LowerBar(UnderLap)', 1500, 350, '000000')
+	setObjectCamera('LowerBar(UnderLap)', 'HUD')
+	addLuaSprite('LowerBar(UnderLap)', false)
+
+    UpperBar = getProperty('UpperBar(UnderLap).y')
+	LowerBar = getProperty('LowerBar(UnderLap).y')
+
+    for Notes = 0,7 do 
+        StrumY = getPropertyFromGroup('strumLineNotes', Notes, 'y')
+    end
 end
 
-function onUpdate()
-	-- START
-	if startValue == 1 then
-		doTweenY('TopBarStart', 'topBar', 0, 0.5, 'sineOut')
-		doTweenY('BottomBarStart', 'bottomBar', 600, 0.5, 'sineOut')
-	end
 
-	if downscroll and startValue == 1 then
-		doTweenY('TopBarStart', 'topBar', 0, 0.5, 'sineOut')
-		doTweenY('BottomBarStart', 'bottomBar', 600, 0.5, 'sineOut')
-	end
+function onEvent(name, value1, value2)
+	if name == 'Cinematics (UnderLap)' then
+	
+		Speed = tonumber(value1)
+		Distance = tonumber(value2)
 
-	-- END
-	if startValue == 2 then
-		doTweenY('TopBarEnd', 'topBar', -120, 0.5, 'sineOut')
-		doTweenY('BottomBarEnd', 'bottomBar', 720, 0.5, 'sineOut')
-	end
+--ENTRANCES
 
-	if downscroll and startValue == 2 then
-		doTweenY('TopBarEnd', 'topBar', -120, 0.5, 'sineOut')
-		doTweenY('BottomBarEnd', 'bottomBar', 720, 0.5, 'sineOut')
-	end
-end
+		if Speed and Distance > 0 then
 
-function onEvent(name, value1)
-	-- Event Working
-	if name == 'Cinematic_Bars' then
-		startValue = tonumber(value1)
+			doTweenY('UnderLap1', 'UpperBar(UnderLap)', UpperBar + Distance, Speed, 'QuadOut')
+			doTweenY('UnderLap2', 'LowerBar(UnderLap)', LowerBar - Distance, Speed, 'QuadOut')
+
+			for Tweens = 0,7 do 
+				noteTweenY('MoveIn(UnderLap)'..Tweens, Tweens, (StrumY + Distance) - 35, Speed, 'QuadOut')
+
+				for Alphas = 1,8 do
+					doTweenAlpha('Alpha(UnderLap)'..Alphas, HudAssets[Index], 0, Speed - 0.1)
+					Index = Index + 1
+						
+					if Index > #HudAssets then
+						Index = 1
+					end
+				end
+			end
+		end
+
+		if downscroll and Speed and Distance > 0 then
+		
+			doTweenY('UnderLap1', 'UpperBar(UnderLap)', UpperBar + Distance, Speed, 'QuadOut')
+			doTweenY('UnderLap2', 'LowerBar(UnderLap)', LowerBar - Distance, Speed, 'QuadOut')
+
+				for Tweens = 0,7 do 
+					noteTweenY('MoveIn(UnderLap)'..Tweens, Tweens, (StrumY - Distance) + 35, Speed, 'QuadOut')
+			
+					for Alphas = 1,8 do
+						doTweenAlpha('Alpha(UnderLap)'..Alphas, HudAssets[Index], 0, Speed - 0.1)
+						Index = Index + 1
+							
+						if Index > #HudAssets then
+							Index = 1
+						
+						end
+					end
+				end
+			end
+
+		if Distance <= 0 then
+			
+			doTweenY('UnderLap1', 'UpperBar(UnderLap)', UpperBar, Speed, 'QuadIn')
+			doTweenY('UnderLap2', 'LowerBar(UnderLap)', LowerBar, Speed, 'QuadIn')
+
+			for Tweens = 0,7 do 
+				noteTweenY('MoveOut(UnderLap)'..Tweens, Tweens, StrumY , Speed, 'QuadIn')
+			
+				for Alphas = 1,8 do
+					doTweenAlpha('Alpha(UnderLap)'..Alphas, HudAssets[Index], 1, Speed + 0.1)
+					Index = Index + 1
+							
+					if Index > #HudAssets then
+						Index = 1
+						
+					end
+				end
+			end
+		end	
 	end
 end
