b352531f77ad8cdbb98b9b85863215d63993170b
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-08 13:52:14 -0400

    dont even

--- export/release/windows/bin/assets/custom_events/CharactersSuchAndSuch.lua
+++
@@ -1,9 +0,0 @@
-function onEvent(name, value1)
-
-    if name == 'THEEVENTNAME' and value1 == 'true' then
-    
-    doTweenX('MoveChar', 'THEIMAGETAG', POSITION, TIME, 'CircInOut')
-    
-    end
-    
-    end
