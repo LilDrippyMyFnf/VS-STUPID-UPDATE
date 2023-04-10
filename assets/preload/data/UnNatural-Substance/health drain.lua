c6b6fdecef493865fe595cca39b3f0e5c778e4f7
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-10 17:49:43 -0400

    The Native Language

--- assets/preload/data/UnNatural-Substance/health drain.lua
+++ assets/preload/data/UnNatural-Substance/health drain.lua
@@ -1,6 +1,6 @@
 function opponentNoteHit()
     health = getProperty('health')
-    if getProperty('health') > 0.05 then
+    if getProperty('health') > 0.10 then
         setProperty('health', health- 0.020);
     end
 end
