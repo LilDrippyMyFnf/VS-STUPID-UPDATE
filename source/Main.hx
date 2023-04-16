1e2d19af3ef36e5291f83e8d122285efcd3f5ce9
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-04-16 01:16:30 -0400

    screenshot plugin

--- source/Main.hx
+++ source/Main.hx
@@ -104,6 +104,8 @@ class Main extends Sprite
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
 		#end
+
+		FlxG.plugins.add(new screenshotplugin.ScreenShotPlugin());
 		
 		#if CRASH_HANDLER
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
